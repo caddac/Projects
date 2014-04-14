a6eeac054d5803a66cc5ab3d395975cb3904b5a5
Dave <dave.cavaletto@mavs.uta.edu>
2014-04-13 22:22:31 -0500

    added commonStructures, my implementation of common data structures in c#

---
+++ CommonStructures/LinkedList/Link.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class GenericLinkList<T>
+    {
+        private class Node
+        {
+            private Node _next;
+            private T _data;
+
+            public Node next { get { return _next; } set { _next = value; } }
+            public T data { get { return _data; } set { _data = value; } }
+
+            public Node(T t)
+            {
+                next = null;
+                data = t;
+            }
+        }
+
+        private Node head;
+
+        public GenericLinkList(T data)
+        {
+            head = new Node(data);
+        }
+
+        public GenericLinkList(T[] data)
+        {
+            head = new Node(data[0]);
+            Node ptr = head;
+
+            foreach (T d in data.Skip(1))
+            {
+                ptr.next = new Node(d);
+                ptr = ptr.next;
+            }
+        }
+
+        public void Append(T data)
+        {
+            Node ptr = head;
+            while (ptr.next != null)
+            {
+                ptr = ptr.next;
+            }
+            ptr.next = new Node(data);
+        }
+
+        public IEnumerable<T> GetEnumerator()
+        {
+            Node ptr = head;
+
+            while (ptr.next != null)
+            {
+                yield return ptr.data;
+                ptr = ptr.next;
+            }
+        }
+    }
+}
---
+++ CommonStructures/LinkedList/LinkedListTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class LinkedListTest
+    {
+        static void Main()
+        {
+            GenericLinkList<int> list = new GenericLinkList<int>(new int[] {1,2,3,4,4,5,5,6,});
+
+
+            foreach (int i in list)
+            {
+                System.Console.WriteLine(i + System.Environment.NewLine);
+            }
+        }
+    }
+}
